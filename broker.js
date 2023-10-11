9697a6aa76698367ae17ab53874a4ca9e7257096
Elias <bonojibonoji@gmail.com>
2023-10-11 22:25:12 +0300

    fix typo

--- broker.js
+++ broker.js
@@ -66,7 +66,7 @@ export const createWalletListener = (walletAddress) => {
 			Object.assign(conversions, res.conversions)
 			//console.log("conversions arrived"+"\n"+res)
 		}				
-		if (res[address-transactions] || res[block-transactions]) {
+		if (res["address-transactions"] || res["block-transactions"]) {
 			const { balance, mempoolIn, mempoolOut } = await getActualBalance(walletAddress)
 			await dbQueries.updateWalletBalance(walletAddress, balance, mempoolIn, mempoolOut)
 			const text = statusText(walletAddress, balance, mempoolIn, mempoolOut)
